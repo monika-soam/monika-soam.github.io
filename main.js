1280d039d7f193f2aa2a1cfbef63296c0989cbe7
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-02-25 03:05:21 +0530

    Added linter files and also created one main.js file to work with requirements

---
+++ main.js
@@ -0,0 +1,7 @@
+const btnHeader = document.getElementById('btn-header');
+const divMenu = document.getElementById('menu-bar');
+const hamBurger = document.getElementById('hamburger');
+
+btnHeader.addEventListener('click', () => {
+
+});
