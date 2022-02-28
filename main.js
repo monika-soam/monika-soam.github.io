e40f91dc70cffd3ea42b297f350918f46b96f5e9
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-02-28 18:51:20 +0530

    Created the branch portfolio-mobile-menu and created new div for menu and also changed style

--- main.js
+++
@@ -1,7 +0,0 @@
-const btnHeader = document.getElementById('btn-header');
-const divMenu = document.getElementById('menu-bar');
-const hamBurger = document.getElementById('hamburger');
-
-btnHeader.addEventListener('click', () => {
-
-});
