5dfee394873a8fe71f6c2b548f2326d0316458dc
Alfred <kalumunabisimwa5@gmail.com>
2022-02-28 14:55:14 +0200

    Add function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const btnHeader = document.getElementById("btn-header");
 const divMenu = document.getElementById("menu-bar");
+function hideMenu() {
+  divMenu.style.display = "none";
+}
+btnHeader.addEventListener("click", () => {
+  hideMenu();
+});
