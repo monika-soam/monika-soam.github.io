4e237588ce9c01c1c7341e934fc2fb0fc3e474f8
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-03-02 17:54:45 +0530

    Changes the form section r=to validate it

--- script.js
+++ script.js
@@ -2,71 +2,72 @@ const btnHeader = document.getElementById('btn-header');
 const divMenu = document.getElementById('menu-bar');
 const hamBurger = document.getElementById('hamburger');
 const menuBarContent = document.querySelector('.menu_bar_content');
+
 const projects = [{
-  featuredImage: 'img/Evocode.svg',
-  name: 'Monika',
-  title: 'Tonic',
-  description: {
-    interface: 'Back End Dev',
-    year: 2022,
+    featuredImage: 'img/Evocode.svg',
+    name: 'Monika',
+    title: 'Tonic',
+    description: {
+      interface: 'Back End Dev',
+      year: 2022,
+    },
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript'],
+    otherTechnologies: ['github', 'ruby', 'bootstrap'],
+
+    liveDemo: '',
+    sourceCode: '',
   },
-  fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  technologies: ['Html', 'Css', 'Javascript'],
-  otherTechnologies: ['github', 'ruby', 'bootstrap'],
 
-  liveDemo: '',
-  sourceCode: '',
-},
+  {
+    featuredImage: 'img/GYMFit.svg',
+    name: 'Monika',
+    title: 'Tonic',
+    description: {
+      interface: 'Back End Dev',
+      year: 2022,
+    },
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript'],
+    otherTechnologies: ['github', 'ruby', 'bootstrap'],
 
-{
-  featuredImage: 'img/GYMFit.svg',
-  name: 'Monika',
-  title: 'Tonic',
-  description: {
-    interface: 'Back End Dev',
-    year: 2022,
+    liveDemo: '',
+    sourceCode: '',
   },
-  fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  technologies: ['Html', 'Css', 'Javascript'],
-  otherTechnologies: ['github', 'ruby', 'bootstrap'],
+  {
+    featuredImage: './img/Avalability.svg',
+    name: 'Monika',
+    title: 'Multi-Post Stories',
+    description: {
+      interface: 'Back End Dev',
+      year: 2022,
+    },
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript'],
+    otherTechnologies: ['github', 'ruby', 'bootstrap'],
 
-  liveDemo: '',
-  sourceCode: '',
-},
-{
-  featuredImage: './img/Avalability.svg',
-  name: 'Monika',
-  title: 'Multi-Post Stories',
-  description: {
-    interface: 'Back End Dev',
-    year: 2022,
+    liveDemo: '',
+    sourceCode: '',
   },
-  fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  technologies: ['Html', 'Css', 'Javascript'],
-  otherTechnologies: ['github', 'ruby', 'bootstrap'],
+  {
+    featuredImage: 'img/ProfessionalArt.svg',
+    name: 'Monika',
+    title: 'Tonic',
+    description: {
+      interface: 'Back End Dev',
+      year: 2022,
+    },
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript'],
+    otherTechnologies: ['github', 'ruby', 'bootstrap'],
 
-  liveDemo: '',
-  sourceCode: '',
-},
-{
-  featuredImage: 'img/ProfessionalArt.svg',
-  name: 'Monika',
-  title: 'Tonic',
-  description: {
-    interface: 'Back End Dev',
-    year: 2022,
+    liveDemo: '',
+    sourceCode: '',
   },
-  fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  detail: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  technologies: ['Html', 'Css', 'Javascript'],
-  otherTechnologies: ['github', 'ruby', 'bootstrap'],
-
-  liveDemo: '',
-  sourceCode: '',
-},
 
 ];
 
